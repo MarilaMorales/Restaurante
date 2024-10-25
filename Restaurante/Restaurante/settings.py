a13f42be5c64e45840b17e1d16d9d7913d69f5fa
Jenny-G15 <jguadamuz@fwdcostarica.com>
2024-10-25 11:28:35 -0600

    base

--- Restaurante/Restaurante/settings.py
+++ Restaurante/Restaurante/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'Restaurante',  # El nombre de tu base de datos en MySQL
         'USER': 'marila',  # Tu usuario de MySQL
         'PASSWORD': 'Paz12',  # La contraseña de tu usuario MySQL
-        'HOST': '127.0.0.1',  # Si estás ejecutando MySQL localmente
+        'HOST': '192.168.100.116',  # Si estás ejecutando MySQL localmente
         'PORT': '3306',  # El puerto por defecto de MySQL
     }
 }
